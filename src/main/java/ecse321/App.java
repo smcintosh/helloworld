49634ec3531eb7f799f0953e028704d064280aa9
mjtucker <35466823+mjtucker@users.noreply.github.com>
2018-09-12 13:58:56 -0400

    Italian Translation Fixed
    Closes #1

--- src/main/java/ecse321/App.java
+++ src/main/java/ecse321/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Ciao Mondo!" );
+        System.out.println("Ciao Mondo!");
         System.out.println("Hello MJ");
     }
 }
