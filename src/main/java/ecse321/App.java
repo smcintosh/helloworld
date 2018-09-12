cd003ae0251a34027c9ed5cadad26a82a84e38bd
Rintaro Nomura <norinmura@MacBook-Pro.local>
2018-09-12 13:56:48 -0400

    Closes #1

--- src/main/java/ecse321/App.java
+++ src/main/java/ecse321/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         for (int i=1; i<1001; i++) {
-            System.out.println( "Hello World!" );
+            System.out.println( "Hello, World!" );
         }
     }
 }
