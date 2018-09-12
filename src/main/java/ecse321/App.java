f205c6f4af2f612eb17b914ebc2f222e1b54423c
Noam Suissa <noam.suissa01@gmail.com>
2018-09-12 14:06:31 -0400

    Closes #1
    translated message to Italian

--- src/main/java/ecse321/App.java
+++ src/main/java/ecse321/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("Scoop Diddy Woop");
+        System.out.println( "Ciao Mondo!" );
     }
 }
