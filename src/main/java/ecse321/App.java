838e50c9baea4d8f057ca7f112009f332ddbaf60
rog22rz <roger.zhang@mail.mcgill.ca>
2018-09-12 14:09:53 -0400

    Closes#1

--- src/main/java/ecse321/App.java
+++ src/main/java/ecse321/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
        for(int i = 0; i < 5; i++){
-	 System.out.println( "Capn crunch > Frosted Flakes" );
+	 System.out.println( "Marcella Hazan >" );
 	}
     }
 }
