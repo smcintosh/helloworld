2af741c1437af77cbb26623db3c4cd4785c875fb
Thomas Bahen <tombahen@gmail.com>
2018-09-12 16:42:48 -0400

    Fixed the ; error

--- src/main/java/ecse321/App.java
+++ src/main/java/ecse321/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         //Intentional Build Break
-        System.out.println( "Ciao Mondo" )
+        System.out.println( "Ciao Mondo" );
 
         System.out.println( "I'm super stoked for ECSE321!");
     }
