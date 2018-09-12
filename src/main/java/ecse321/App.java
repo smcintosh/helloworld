bdca30469f83272d408811ec5562a6280e4f95ab
Murray Kornelsen <murray.kornelsen@mail.mcgill.ca>
2018-09-12 13:57:03 -0400

    Closes #1

--- src/main/java/ecse321/App.java
+++ src/main/java/ecse321/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         while (true) {
-            System.out.println("Hello World!");
+            System.out.println("Ciao Mondo!");
         }
         
     }
