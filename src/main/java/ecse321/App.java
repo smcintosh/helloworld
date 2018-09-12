2080eb5a91a3410b1fdd99d19576e0d712590f4f
Connor Plante <connor.plante@mail.mcgill.ca>
2018-09-12 13:58:38 -0400

    Closes #1

--- src/main/java/ecse321/App.java
+++ src/main/java/ecse321/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello, can you hear me?" );
+        System.out.println( "Ciao Mondo!" );
     }
 }
