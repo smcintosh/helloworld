6e6229772ada3a0b5f09dc610abe9e7b6fe70c16
RyanDotsikas <ryan.dotsikas@mail.mcgill.ca>
2018-09-12 13:57:01 -0400

    Change language to italian

--- src/main/java/ecse321/App.java
+++ src/main/java/ecse321/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Ryan Dotsikas says: Hello World!" );
+        System.out.println( "Ciao mondo" );
     }
 }
