09d860aad169ada0aa664f416016080e0a3a85d2
Brendan Kellam <bshizzle1234@gmail.com>
2018-09-12 21:34:42 -0400

    Introduced error to test Travis CI

--- src/main/java/ecse321/App.java
+++ src/main/java/ecse321/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Ciao mondo!" );
         System.out.println( "C++ > Java" );
+        System.out.println("Build break. Will forget a semi-colon here!")
     }
 }
