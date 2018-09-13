571f39c3a856b6a8c5d271f4b6ad7514d49c5818
RyanDotsikas <ryan.dotsikas@mail.mcgill.ca>
2018-09-13 14:18:18 -0400

    Added the semicolon again

--- src/main/java/ecse321/App.java
+++ src/main/java/ecse321/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Ryan Dotsikas says: Ciao mondo!" )
+        System.out.println( "Ryan Dotsikas says: Ciao mondo!" );
     }
 }
