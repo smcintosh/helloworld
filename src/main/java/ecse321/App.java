4475afd02a2a5d7bc879cd29d01b7bb746b68643
afif hendrawan <afif.hendrawan@gmail.com>
2018-10-06 21:51:44 +0700

    Indonesia translation

--- src/main/java/ecse321/App.java
+++ src/main/java/ecse321/App.java
@@ -1,13 +1,13 @@
 package ecse321;
 
 /**
- * Hello world!
+ * Halo Dunia!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Halo Dunia!" );
     }
 }
