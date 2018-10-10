539cdff30258544dd4ed6f7de44b007ca6409754
mjtucker <35466823+mjtucker@users.noreply.github.com>
2018-10-09 21:47:11 -0400

    Update App.java
    Closes #2

--- src/main/java/ecse321/App.java
+++ src/main/java/ecse321/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("Ciao Mondo!");
-        System.out.println("Hello dood");
+        System.out.println("Hello dood!");
     }
 }
