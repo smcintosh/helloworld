5968a7255dd757c5164d8d9c08ba5d2d3e494d30
mjtucker <35466823+mjtucker@users.noreply.github.com>
2018-10-09 21:46:20 -0400

    Update App.java

--- src/main/java/ecse321/App.java
+++ src/main/java/ecse321/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("Ciao Mondo!");
-        System.out.println("Hello MJ");
+        System.out.println("Hello dood");
     }
 }
